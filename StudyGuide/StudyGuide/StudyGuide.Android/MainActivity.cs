789ce45cfe9ee29bb52e406869b0e0a8c7432853
EORG-Dev <stoptv2014@gmail.com>
2023-11-14 22:54:41 +0100

    Projektmappe erstellt
    - Erstellung einer leeren Projektmappe
    - Hinzufügen des Nuget Sharpnado für die Navigation
    - Dateiverzeichnis als Parameter für den Constructor des Shared Projects.

---
+++ StudyGuide/StudyGuide/StudyGuide.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace StudyGuide.Droid
+{
+    [Activity(Label = "StudyGuide", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightYes;
+
+            string folderpath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+
+            Sharpnado.Tabs.Initializer.Initialize(false, false);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App(folderpath));
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ StudyGuide/StudyGuide/StudyGuide/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudyGuide
+{
+    public partial class App : Application
+    {
+        public App(string folderpath)
+        {
+            InitializeComponent();
+            Sharpnado.Tabs.Initializer.Initialize(false, true);
+
+            MainPage = null;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
