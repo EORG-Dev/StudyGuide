e4b8ec73b54773c464df2a339172fa09600a775a
EORG-Dev <stoptv2014@gmail.com>
2023-11-15 19:31:00 +0100

    C_Einsatz Parameter
    Minor:
    - Namespace Korrektur C_Einsatz
    - C_Einsatz-Properties geändert (ohne Liste der Beobachtungen)

--- StudyGuide/StudyGuide/StudyGuide/Models/C_Einsatz.cs
+++ StudyGuide/StudyGuide/StudyGuide/Models/C_Einsatz.cs
@@ -8,7 +8,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proj_AzubiGuide.Models
+namespace StudyGuide.Models
 {
     [DataContract]
     public class C_Einsatz
@@ -27,168 +27,32 @@ namespace Proj_AzubiGuide.Models
             set => Datum = value.Ticks;
         }
         [DataMember]
-        public int RW { get; set; }  
-        public bool IsRW 
-        { 
-            get 
-            {
-                return RW != 555;
-            } 
-        }
-        public bool IsNotRW
-        {
-            get
-            {
-                return !IsRW;
-            }
-        }
-
+        public string Rettungsmittel { get;set; }  
         [DataMember]
-        public int Rettungsmittel { get;set; }  
-        [DataMember]
-        public int Stichwort_RM { get; set; }  
-        [DataMember]
-        public int Stichwort { get; set; }  
-        /*public string S_StichwortGesamt
-        {
-            get => Srv_Options.GetItemName(Stichwort_RM) + "-" + Srv_Options.GetItemName(Stichwort);
-        }*/
-        [DataMember]
-        public int Fehleinsatz { get; set; }
-        [DataMember]
-        public string Funkrufname { get; set; } = "";
-        [DataMember]
-        public int FF { get; set; }
-        [DataMember]
-        public string FF_Name { get; set; } 
-        [DataMember] 
-        public byte[] Unterschrift { get; set; }
-        public bool HasUnterschrift
-        {
-            get
-            {
-                return Unterschrift?.Length > 0;
-            }
-        }
-        [DataMember]
-        public bool ZumPatMitSoSi { get; set; } = true;
-        [DataMember]
-        public bool MitNa { get; set; }
-        [DataMember]
-        public int Schichtart { get; set; }
+        public string Alarmtext { get; set; }  
         #endregion ED
-        #region Beteiligung
-        [DataMember]
-        public bool Telefon { get; set; }
-        [DataMember]
-        public bool Beifahrer { get; set; }
-        [DataMember]
-        public bool Anreichen { get; set; }
-        [DataMember]
-        public bool Diagnostik { get; set; }
-        [DataMember]
-        public bool Anamnese { get; set; }
-        [DataMember]
-        public bool TherapieB { get; set; }
-        [DataMember]
-        public bool Transport { get; set; }
-        #endregion Beteiligung
         #region SSS
         [DataMember]
-        public int Einsatzort { get; set; }  
+        public string Einsatzort { get; set; }  
         [DataMember]
-        public int Situation { get; set; }
-        [Ignore]
-        public List<int> Safety
-        {
-            get => GetBlob(SafetyBlobbed);
-            set => SafetyBlobbed = SetBlob(value);
-        }
-        [DataMember]
-        public string SafetyBlobbed { get; set; }
-        [DataMember]
-        public int Tageszeit { get; set; }  
+        public string Situation { get; set; }
         #endregion SSS
         #region Patient
         [DataMember]
-        public int Geschlecht { get; set; }
+        public object Geschlecht { get; set; } = "";
         [DataMember]
         public int Alter { get; set; } = -1;
-        /*public string S_AlterGeschlecht
-        {
-            get
-            {
-                return $"{Alter}, {Srv_Options.GetItemName(Geschlecht)}";
-            }
-        }*/
-        [DataMember]
-        public string Altersgruppe { get; set; }  
-        [DataMember]
-        public int Lebenssituation { get; set; }
-        //public int AnzahlVorerkrankungen { get; set; } = -1;
-        [Ignore]
-        public List<int> Diagnosen
-        {
-            get => GetBlob(DiagnosenBlobbed);
-            set => DiagnosenBlobbed = SetBlob(value);
-        }
-        [DataMember]
-        public string DiagnosenBlobbed { get; set; }
-        /*public string ErsteDiagnose
-        {
-            get => Srv_Options.GetItemName(Diagnosen.FirstOrDefault(), "unklar");
-        }
-        public string ErsteZweiDiagnose
-        {
-            get
-            {
-                if (Diagnosen.Count < 2 || Diagnosen == null) return ErsteDiagnose;
-                return Srv_Options.GetItemName(Diagnosen[0]) + ", " + Srv_Options.GetItemName(Diagnosen[1]);
-            }
-        }*/
-        [DataMember]
-        public int Therapie { get; set; }
-        [Ignore]
-        public List<int> BesonderM
-        {
-            get => GetBlob(BesondereMBlobbed);
-            set => BesondereMBlobbed = SetBlob(value);
-        }
-        [DataMember]
-        public string BesondereMBlobbed { get; set; }
-        [Ignore]
-        public List<int> Medikamente
-        {
-            get => GetBlob(MedikamenteBlobbed);
-            set => MedikamenteBlobbed = SetBlob(value);
-        }
-        [DataMember]
-        public string MedikamenteBlobbed { get; set; }
         #endregion Patient
+        #region Tatigkeiten
+        // Hier Liste mit Beobachtungen, dafür neue Klasse
+        #endregion Tatigkeiten
         #region Auswertung
         [DataMember]
-        public int NACA { get; set; }
+        public string RDVersorgung { get; set; }
         [DataMember]
-        public int RDVersorgung { get; set; }  
-        [DataMember]
-        public bool TrspMitSoSi { get; set; } = false;
-        [DataMember]
-        public int NKat { get; set; }
-        [Ignore]
-        public List<int> Einsatzbesonderheiten
-        {
-            get => GetBlob(EinsatzbesonderheitenBlobbed);
-            set => EinsatzbesonderheitenBlobbed = SetBlob(value);
-        }
-        [DataMember]
-        public string EinsatzbesonderheitenBlobbed { get; set; }
-
+        public object EinweisungDurch { get; set; } = "";
         #endregion Auswertung
         #region Sonstiges
-        [DataMember]
-        public string Sonstiges { get; set; } = "";
-        [DataMember]
-        public bool Einsatzbericht { get; set; }
         #endregion Sonstiges
         List<int> GetBlob(string s)
         {
---
+++ StudyGuide/StudyGuide/StudyGuide/Services/Srv_Data.cs
@@ -0,0 +1,130 @@
+﻿using StudyGuide.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGuide.Services
+{
+    public static class Srv_Data
+    {
+        public static void Setup()
+        {
+            if (!File.Exists(Singleton.Instance.DataFilePath))
+            {
+                using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+                {
+                    con.CreateTable<C_Einsatz>();
+                }
+            }
+        }
+        public static C_Einsatz GetPreset()
+        {
+            // Get last or return new
+            var last = GetLastEinsatz();
+            if (last == null)
+            {
+                return new C_Einsatz();
+            }
+
+            // copy certain values from last to new
+            var neu = new C_Einsatz();
+            neu.RW = last.RW;
+            neu.Rettungsmittel = last.Rettungsmittel;
+
+            // return new
+            return neu;
+        }
+        #region REST
+        public static ObservableCollection<T> GetAll<T>() where T : new()
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                var res = new ObservableCollection<T>();
+                var list = con.Table<T>().ToList();
+                list.ForEach(t => res.Add(t));
+                return res;
+            }
+        }
+        public static C_Einsatz GetLastEinsatz()
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                List<C_Einsatz> res = con.Table<C_Einsatz>().OrderBy(X => X.Erstellt).ToList();
+                return res.LastOrDefault();
+            }
+        }
+        public static C_Einsatz GetByID(string ID)
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                var res = con.Query<C_Einsatz>($"SELECT * FROM C_Einsatz WHERE ID = \"{ID}\"");
+                return res.FirstOrDefault();
+            }
+        }
+        public static bool InsertAll<T>(List<object> Insert) where T : class
+        {
+            foreach (object o in Insert)
+            {
+                Insert<T>(o);
+            }
+            return true;
+        }
+        public static bool Insert<T>(object Insert) where T : class
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                con.Insert(Insert as T);
+                return true;
+            }
+        }
+        public static bool InsertOrReplace<T>(object Insert) where T : class
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                con.InsertOrReplace(Insert as T);
+                return true;
+            }
+        }
+        public static bool Remove<T>(object Delete) where T : class
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                con.Delete(Delete as T);
+                return true;
+            }
+        }
+        #endregion REST
+        public static List<(long, List<C_Einsatz>)> GroupByDienste()
+        {
+            using (SQLite.SQLiteConnection con = new SQLite.SQLiteConnection(Singleton.Instance.DataFilePath))
+            {
+                List<(long, List<C_Einsatz>)> res = new List<(long, List<C_Einsatz>)>();
+                var groups = con.Table<C_Einsatz>().GroupBy(X => X.S_Datum);
+                foreach (var g in groups.ToList())
+                {
+                    DateTime dtdate = new DateTime();
+                    DateTime.TryParse(g.Key, out dtdate);
+                    long date = dtdate.Ticks;
+                    List<C_Einsatz> lE = new List<C_Einsatz>();
+                    foreach (var gg in g)
+                    {
+                        lE.Add(gg);
+                    }
+                    res.Add((date, lE));
+                }
+                return res;
+            }
+        }
+        public static List<C_Einsatz> GetLastDienst()
+        {
+            var dienste = GroupByDienste();
+            var dienstdate = dienste.Max(X => X.Item1);
+            var dienst = dienste.Where(X => X.Item1 == dienstdate).FirstOrDefault();
+            return dienst.Item2 ?? null;
+        }
+    }
+}
